2523f6b56c4e7e63678d84495fe0587b3b0ec8e9
dirkarnez <smalldirkalex@gmail.com>
2026-02-03 16:59:12 +0800

    - wip

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,19 +1,18 @@
 // MyLibrary.hello("dsf")
-// declare global {
-//   interface Document {
-//     customProperty?: string;
-//     customMethod?(): void;
-//   }
-// }
+declare global {
+  interface Document {
+    customProperty?: string;
+    customMethod?(): void;
+  }
+}
+const nthQuestion = nth => document.querySelector(`#question-list > div:nth-child(${nth}) input`);
+
+// document.querySelector("#question-list > div:nth-child(5) span").innerText
 
 export function main(name: string, hasVisualizer: boolean, hasCeilingMic: boolean, hasLecternLamp: boolean) {
   alert(name);
-  const element = document.querySelector("#question-list > div:nth-child(2)");
-  if (element) {
-    const intputElementCollection: HTMLCollectionOf<HTMLInputElement> = element.getElementsByTagName("input");
-    if (intputElementCollection.length == 1) {
-      const c: HTMLInputElement = intputElementCollection[0];
-      c.value = `123`;
-    }
-  }
+  const input = nthQuestion(0);
+  const input = nthQuestion(0);
+  const input = nthQuestion(0);
+  const input = nthQuestion(0);
 }
