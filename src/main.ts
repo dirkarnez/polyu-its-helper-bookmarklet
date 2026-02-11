a7e841b1f47494da91548288e12830d5b9449b11
Dirk Arnez <smalldirkalex@gmail.com>
2026-02-11 16:56:20 +0800

    - update main.ts

--- src/main.ts
+++ src/main.ts
@@ -42,7 +42,7 @@ const rooms: Room[] = [
   new Room({ name: "HHBUG01", hasVisualizer: false, hasCeilingMic: false, hasLecternLamp: false, numberOfWirelessMics: 2 }),
   new Room({ name: "HHBUG03", hasVisualizer: false, hasCeilingMic: true, hasLecternLamp: false, numberOfWirelessMics: NaN }),
   new Room({ name: "HHBUG04", hasVisualizer: false, hasCeilingMic: false, hasLecternLamp: false, numberOfWirelessMics: 2 }),
-  new Room({ name: "HHBUG05", hasVisualizer: false, hasCeilingMic: true, hasLecternLamp: false, numberOfWirelessMics: NaN }),
+  new Room({ name: "HHBUG05", hasVisualizer: false, hasCeilingMic: false, hasLecternLamp: false, numberOfWirelessMics: 2 }),
   new Room({ name: "HHBUG06", hasVisualizer: false, hasCeilingMic: false, hasLecternLamp: false, numberOfWirelessMics: 2 }),
   new Room({ name: "HHBUG09", hasVisualizer: false, hasCeilingMic: true, hasLecternLamp: false, numberOfWirelessMics: NaN }),
   new Room({ name: "HHBUG10", hasVisualizer: false, hasCeilingMic: true, hasLecternLamp: false, numberOfWirelessMics: NaN }),
